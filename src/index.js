fccf3d6dcf04d44a74587c1c96087922b709b535
Osandahimsara <osandahimsara2016@gmail.com>
2025-07-22 13:22:43 +0530

    Add static file serving and catch-all route for React app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+const path = require('path');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Catch-all route to serve React index.html for any unknown paths
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
